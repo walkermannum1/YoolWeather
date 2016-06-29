650f2ea2842dbc73a7ee71b8429cf552d199d3db
walkermannum1 <2469377884@qq.com>
2016-06-29 17:32:24 +0800

    Create table.

---
+++ app/src/main/java/com/example/user/yoolweather/db/YoolWeatherOpenHelper.java
@@ -0,0 +1,48 @@
+package com.example.user.yoolweather.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.security.PublicKey;
+
+/**
+ * Created by user on 2016/6/29.
+ */
+public class YoolWeatherOpenHelper extends SQLiteOpenHelper {
+
+    //Create table Province
+    public static final String CREATE_PROVIBCE = "create table Province ("
+            + "id integer primary key autoincrement, "
+            + "province_name text, "
+            + "province_code text ";
+
+    //Create table City
+    public static final String CREATE_CITY = "create table City ("
+            + "id integer primary key autoincrement, "
+            + "city_name text, "
+            + "city_code text, "
+            + "province_id integer";
+
+    //Create table County
+    public static final String CREATE_COUNTY = "create table County ("
+            + "id integer primary key autoincrement, "
+            + "county_name text, "
+            + "county_code text, "
+            + "city_id integer";
+    public YoolWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PROVIBCE);
+        db.execSQL(CREATE_CITY);
+        db.execSQL(CREATE_COUNTY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
