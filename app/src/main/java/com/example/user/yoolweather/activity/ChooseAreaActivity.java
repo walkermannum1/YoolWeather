b2680ea9648cc93d5e81ec33f909dbba38d5d9ec
walkermannum1 <2469377884@qq.com>
2016-06-30 17:42:10 +0800

    Add some activity code.

--- app/src/main/java/com/example/user/yoolweather/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/example/user/yoolweather/activity/ChooseAreaActivity.java
@@ -2,10 +2,17 @@ package com.example.user.yoolweather.activity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.user.yoolweather.R;
+import com.example.user.yoolweather.model.City;
+import com.example.user.yoolweather.model.County;
+import com.example.user.yoolweather.model.Province;
 import com.example.user.yoolweather.model.YoolWeatherDB;
 
 import java.util.ArrayList;
@@ -27,4 +34,28 @@ public class ChooseAreaActivity extends Activity {
     private YoolWeatherDB yoolWeatherDB;
     private List<String> dataList = new ArrayList<String>();
 
+    private List<Province> provinceList;
+
+    private List<City> cityList;
+
+    private List<County> countyList;
+
+    private Province selectedProvince;
+
+    private City selectedCity;
+
+    private County selectedCounty;
+
+    private int currentLevel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.choose_area);
+        listView = (ListView) findViewById(R.id.list_view);
+        titleText = (TextView) findViewById(R.id.title_text);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataList);
+        listView.setAdapter(adapter);
+    }
 }
