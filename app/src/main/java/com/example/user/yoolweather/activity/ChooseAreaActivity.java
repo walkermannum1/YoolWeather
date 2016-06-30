9d42fa31ee8ac3c4b583802b3f61d6b88c506b60
walkermannum1 <2469377884@qq.com>
2016-06-30 15:54:51 +0800

    Add choosearea.

---
+++ app/src/main/java/com/example/user/yoolweather/activity/ChooseAreaActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.yoolweather.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by user on 2016/6/30.
+ */
+public class ChooseAreaActivity extends Activity {
+
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+
+}
---
+++ app/src/main/java/com/example/user/yoolweather/util/Utility.java
@@ -0,0 +1,69 @@
+package com.example.user.yoolweather.util;
+
+import android.os.Parcelable;
+import android.text.TextUtils;
+
+import com.example.user.yoolweather.model.City;
+import com.example.user.yoolweather.model.County;
+import com.example.user.yoolweather.model.Province;
+import com.example.user.yoolweather.model.YoolWeatherDB;
+
+/**
+ * Created by user on 2016/6/30.
+ */
+public class Utility {
+
+    //Resolve and handle data back from server
+    public synchronized static boolean handleProvinceResponse(YoolWeatherDB yoolWeatherDB, String response) {
+        if (!TextUtils.isEmpty(response)) {
+            String[] allProvinces = response.split(",");
+            if (allProvinces != null && allProvinces.length > 0) {
+                for (String p:allProvinces) {
+                    String[] array = p.split("\\|");
+                    Province province = new Province();
+                    province.setProvinceCode(array[0]);
+                    province.setProvinceName(array[1]);
+                    yoolWeatherDB.saveProvince(province);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCitiesResponse(YoolWeatherDB yoolWeatherDB, String response, int provinceId) {
+        if (!TextUtils.isEmpty(response)) {
+            String[] allCities = response.split(",");
+            if (allCities != null && allCities.length > 0) {
+                for (String c:allCities) {
+                    String[] array = c.split("\\|");
+                    City city = new City();
+                    city.setCityCode(array[0]);
+                    city.setCityName(array[1]);
+                    city.setProvinceId(provinceId);
+                    yoolWeatherDB.saveCity(city);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCountiesResponse(YoolWeatherDB yoolWeatherDB, String response, int cityId) {
+        if (!TextUtils.isEmpty(response)) {
+            String[] allCounties = response.split(",");
+            if (allCounties != null && allCounties.length > 0) {
+                for (String c:allCounties) {
+                    String[] array = c.split("\\|");
+                    County county = new County();
+                    county.setCountyCode(array[0]);
+                    county.setCountyName(array[1]);
+                    county.setCityId(cityId);
+                    yoolWeatherDB.saveCounty(county);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
