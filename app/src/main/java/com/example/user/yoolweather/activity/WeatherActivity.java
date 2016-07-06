39e9583f7673228fad9865c668b991f515332ad8
walkermannum1 <2469377884@qq.com>
2016-07-06 17:42:48 +0800

    Add weather activity code.

--- app/src/main/java/com/example/user/yoolweather/activity/WeatherActivity.java
+++ app/src/main/java/com/example/user/yoolweather/activity/WeatherActivity.java
@@ -1,7 +1,12 @@
 package com.example.user.yoolweather.activity;
 
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -40,14 +45,44 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
         temp1Text = (TextView) findViewById(R.id.temp1);
         temp2Text = (TextView) findViewById(R.id.temp2);
         currentDataText = (TextView) findViewById(R.id.current_data);
-        switchCity = (Button) findViewById(R.id.switch_city)
+        switchCity = (Button) findViewById(R.id.switch_city);
+        refreshWeather = (Button) findViewById(R.id.refresh_weather);
+        String countyCode = getIntent().getStringExtra("county_code");
+        if (!TextUtils.isEmpty(countyCode)) {
+            publishText.setText("synchronizing...");
+            weatherInfoLayout.setVisibility(View.INVISIBLE);
+            cityNameText.setVisibility(View.INVISIBLE);
+            queryWeatherCode(countyCode);
+        } else {
+            showWeather();
+        }
+        switchCity.setOnClickListener(this);
+        refreshWeather.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-
+            case R.id.switch_city:
+                Intent intent = new Intent(this, ChooseAreaActivity.class);
+                intent.putExtra("from_weather_activity", true);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.refresh_weather:
+                publishText.setText("synchronizing...");
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+                String weatherCode = prefs.getString("weather_code", "");
+                if (!TextUtils.isEmpty(weatherCode)) {
+                    queryWeatherInfo(weatherCode);
+                }
+                break;
+            default:
+                break;
         }
     }
 
+    private void queryWeatherCode(String countyCode) {
+
+    }
 }
