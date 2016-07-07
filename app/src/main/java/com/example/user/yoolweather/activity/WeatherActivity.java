eaedf8a8097b38daf9e14886442715bc20ba8a05
walkermannum1 <2469377884@qq.com>
2016-07-07 16:33:31 +0800

    Add Button activity.

--- app/src/main/java/com/example/user/yoolweather/activity/WeatherActivity.java
+++ app/src/main/java/com/example/user/yoolweather/activity/WeatherActivity.java
@@ -1,7 +1,6 @@
 package com.example.user.yoolweather.activity;
 
 import android.app.Activity;
-import android.app.DownloadManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
